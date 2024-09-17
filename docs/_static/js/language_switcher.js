2610f08c0f6bc7e0ff9e31b188c958f095041010
Hiroshi3pei <3pei@moegi.waseda.jp>
2024-09-17 17:28:51 +0900

    update language swiching

--- docs/_static/js/language_switcher.js
+++ docs/_static/js/language_switcher.js
@@ -3,7 +3,7 @@ function getCurrentLanguageAndPath() {
     const parts = path.split('/');
     
     // 'docs' を探してそのインデックスを取得
-    const docsIndex = parts.indexOf('docs');
+    const docsIndex = parts.indexOf('hiroshi3pei.github.io');
     
     // 英語版かどうかを判定
     const isEnglish = parts[docsIndex + 1] === 'en';
